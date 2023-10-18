4923c9df4dc44c114d32291782f138634dcf6cdd
Dan Connolly <connolly@agoric.com>
2023-10-17 23:51:40 -0500

    oops: forgot docTools.js

---
+++ docTools.js
@@ -0,0 +1,54 @@
+// @ts-check
+
+const { entries } = Object;
+
+/**
+ *
+ * @param {Document} document
+ */
+export const makeDocTools = document => {
+  /** @param {string} sel */
+  const $ = sel => document.querySelector(sel);
+  /** @param {string} name */
+  const $field = name => {
+    const it = $(`*[name=${JSON.stringify(name)}]`);
+    if (!it) throw Error(name);
+    return it;
+  };
+
+  /**
+   *
+   * @param {string} tag
+   * @param {Record<string, string>} [attrs]
+   * @param {Array<string | Element>} [children]
+   */
+  const elt = (tag, attrs = {}, children = []) => {
+    const it = document.createElement(tag);
+    entries(attrs).forEach(([name, value]) => it.setAttribute(name, value));
+    children.forEach(ch => {
+      if (typeof ch === 'string') {
+        it.appendChild(document.createTextNode(ch));
+      } else {
+        it.appendChild(ch);
+      }
+    });
+    return it;
+  };
+
+  /**
+   * @template {string} LP
+   * @template {string} VP
+   * 
+   * @param {SelectElement} selectElt 
+   * @param {Array<Record<LP|VP, string>>} choices 
+   * @param {LP} labelProp 
+   * @param {VP} valueProp 
+   */
+  const setChoices = (selectElt, choices, labelProp, valueProp) => {
+    selectElt.innerHTML = '';
+    choices.forEach(item => {
+        const option = elt('option', {value: item[valueProp]}, [item[labelProp]]);
+    })
+  }
+  return { elt, $, $field, setChoices };
+};
